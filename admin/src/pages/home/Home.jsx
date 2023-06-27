1dbaeef4a771c2666635b1cee409abf63fb1d393
Prachi Meshram <prachimesh89@gmail.com>
2023-06-27 19:10:11 +0530

    bug fixes

---
+++ admin/src/pages/home/Home.jsx
@@ -0,0 +1,21 @@
+import Chart from "../../components/chart/Chart";
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo";
+import "./home.css";
+import { userData } from "../../dummyData";
+import WidgetSm from "../../components/widgetSm/widgetSm";
+import WidgetLg from "../../components/widgetLg/widgetLg";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <FeaturedInfo />
+      <Chart data={userData} title="User Analytics" grid dataKey="Active User"/>
+      <div className="homeWidgets">
+        <WidgetSm/>
+        <WidgetLg/>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
